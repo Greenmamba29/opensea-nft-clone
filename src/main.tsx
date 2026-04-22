76b2ce7ce61aa895b2bea6eb7213e037d237d664
Greenmamba29 <mcdonald@lithiumbuy.com>
2026-04-22 14:28:41 -0400

    fix: move globals.css and simplify build script

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './app/globals.css'
+import './styles/globals.css'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import CollectionPage from './pages/CollectionPage'
